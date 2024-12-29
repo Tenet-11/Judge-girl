0ae7a3b24ffc67f3009fa57a01b41142d7f36246
Tenet-11 <tenet113219@gmail.com>
2024-12-29 16:05:18 +0800

    Create 27.c

---
+++ CH04 Comparison Logic/27.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char input;
+    scanf(" %c", &input);
+    input = toupper(input);
+    if (input < 'A' || input>'Z') {
+        printf("not English.");
+    }
+    else if (input == 'A') {
+        printf("the first.");
+    }
+    else if (input == 'Z') {
+        printf("the last.");
+    }
+    else {
+        printf("%c%c", input - 1, input + 1);
+    }
+    printf("\n");
+}
