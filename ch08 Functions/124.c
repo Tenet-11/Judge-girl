07e6ffe0e9783d0f30219100af13a7aba768de2e
Tenet-11 <tenet113219@gmail.com>
2024-12-30 12:47:27 +0800

    Update 124.c

--- ch08 Functions/124.c
+++ ch08 Functions/124.c
@@ -1,1 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "D_size.h"
 
+int D_size(int S, int D) {
+    while (D > 0) {
+        D -= S;
+        S++;
+    }
+    return S - 1;
+}
